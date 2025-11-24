32c941da3ca3a6b1b47859124470b019505a2209
anilfoss <109171280+anilfoss@users.noreply.github.com>
2025-11-25 01:44:11 +0530

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -59,6 +59,8 @@ const config = {
             boxShadow: {
                 card: "0px 4px 20px 0px rgba(0, 0, 0, 0.1)",
                 "header-2": "0px -2px 0px 0px rgba(0,0,0,0.2) inset",
+                "header-2-white":
+                    "0px -2px 0px 0px rgba(255,255,255,0.1) inset",
             },
         },
     },
